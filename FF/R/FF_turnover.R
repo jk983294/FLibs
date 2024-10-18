01b3da4026f1aa45e69cecb446972088a30df271
jk983294 <jk983294@gmail.com>
2024-10-18 08:32:56 +0800

    acfs

--- FF/R/FF_turnover.R
+++ FF/R/FF_turnover.R
@@ -3,11 +3,33 @@
 #'
 #' @param dt dt, expect factor data
 #' @param qtl_name default "f_qtl"
-#' @param shift period default 1
+#' @param shifts period array default 1
 #'
 #' @import data.table
 #' @export
-factor_turnovers <- function(dt, qtl_name = "f_qtl", shift = 1L) {
-    dt <- setDT(.Call(`_FF_calc_qtl_turnover`, dt, qtl_name, shift))
+factor_turnovers <- function(dt, qtl_name = "f_qtl", shifts = c(1L)) {
+    dt <- rbindlist(lapply(shifts, function(p) {
+        dt1 <- setDT(.Call(`_FF_calc_qtl_turnover`, dt, qtl_name, p))
+        dt1[, shift := p]
+        return(dt1)
+    }))
+    return(dt)
+}
+
+#' factor_rank_acf
+#' Computes autocorrelation of factor ranks
+#'
+#' @param dt dt, expect factor data
+#' @param f_name default "factor"
+#' @param shifts period default 1
+#'
+#' @import data.table
+#' @export
+factor_rank_acf <- function(dt, f_name = "factor", shifts = c(1L)) {
+    dt <- rbindlist(lapply(shifts, function(p) {
+        dt1 <- setDT(.Call(`_FF_calc_rank_acf`, dt, f_name, p))
+        dt1[, shift := p]
+        return(dt1)
+    }))
     return(dt)
 }
--- FF/demo/turnover_demo.R
+++ FF/demo/turnover_demo.R
@@ -2,6 +2,8 @@ library(data.table)
 library(ggplot2)
 
 dt <- FF::dummy_factor()
-tvs <- FF::factor_turnovers(dt)
+tvs <- FF::factor_turnovers(dt, shifts = 1L:5L)
+acfs <- FF::factor_rank_acf(dt, shifts = 1L:5L)
 
-dt[, f_rank := FM::rank(factor), by = .(DataDate, ticktime)]
+tvs[, FM::stats(tvs), by = .(shift, f_qtl)]
+acfs[, FM::stats(acf), by = .(shift)]
